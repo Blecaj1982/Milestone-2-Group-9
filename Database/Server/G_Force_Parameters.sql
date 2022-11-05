7749c373372fc70654be3496d711201a728f67b7
josh murphy <jaymurph92@gmail.com>
2022-11-04 22:29:10 -0400

    Creating SQL Server Project

---
+++ Database/Server/G_Force_Parameters.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [FDMS].[G_Force_Parameters]
+(
+	[Telemetry_ID] BIGINT NOT NULL PRIMARY KEY, 
+    [Accel_X] DECIMAL NOT NULL, 
+    [Accel_Y] DECIMAL NOT NULL, 
+    [Accel_Z] DECIMAL NOT NULL, 
+    CONSTRAINT [FK_G_Force_Parameters_To_Telemetry_Records] FOREIGN KEY ([Telemetry_ID]) REFERENCES [FDMS].[Telemetry_Records]([Telemetry_ID])
+)
---
+++ Database/Server/Telemetry_Records.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [FDMS].[Telemetry_Records]
+(
+	[Telemetry_ID] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Aircraft_Tail_Num] VARCHAR(6) NOT NULL, 
+    [Timestamp] DATETIME NOT NULL , 
+    [Entry_Timestamp] DATETIME NOT NULL DEFAULT getdate()
+)
