464aeead21c90cb6c8f7dfab98bc736d8cef2711
josh murphy <jaymurph92@gmail.com>
2022-11-05 14:40:50 -0400

    Create IFdmsDatabase interface

---
+++ Database/DAL/IFdmsDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDMS.DAL
+{
+    interface IFdmsDatabase
+    {
+        DALResult Connect(string connectionString);
+
+        DALResult Insert(TelemetryRecordDAL record);
+
+        DALSelectResult Select(string aircraftTailNum);
+
+        DALResult Disconnect();
+    }
+
+
+    public class DALSelectResult : DALResult
+    {
+        public List<TelemetryRecordDAL> Records;
+    }
+
+    public class DALResult
+    {
+        public bool Success;
+        public string FailureMessage;
+    }
+}
