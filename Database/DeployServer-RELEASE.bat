ab162d9ccee5e557453650a9167c8787047570fe
josh murphy <jaymurph92@gmail.com>
2022-11-05 21:03:33 -0400

    migrate to package references
    create scripts for deploying FDMS server DACPAC file to SQL Server instance in both release and debug

---
+++ Database/DeployServer-RELEASE.bat
@@ -0,0 +1,9 @@
+
+@ECHO OFF
+
+set /P ip=SQL Server IP :
+set /P port=SQL Server Port :
+set /P id=SQL Server User ID : 
+set /P pass=Sql SErver Password : 
+
+".\sqlPackage\sqlpackage.exe" /Action:Publish /SourceFile:"./Server/bin/Release/FDMS_Server.dacpac" /TargetConnectionString:"Server=tcp:%ip%,%port%;User ID=%id%; Password=%pass%;Initial Catalog=FDMS_Server;"
