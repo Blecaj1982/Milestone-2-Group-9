0e1252e395976e45cd092b181a70357558e144a8
BPLeonVong <bpleonvong@gmail.com>
2022-11-09 05:26:10 -0500

    File Clean Up
    Add and remove console lines and cleaned comments

--- AircraftTransmissionSystem/AircraftTransmissionSystem/AircraftTransmissionSystem.cs
+++ AircraftTransmissionSystem/AircraftTransmissionSystem/AircraftTransmissionSystem.cs
@@ -1,17 +1,16 @@
 ﻿/// 
-/// FILE		    : AircraftTransmissionSystem.cs
-/// PROJECT		    : SENG3020
-/// PROGRAMMER		: SQTeam9
-/// FIRST VERSION	: 11/06/2022
-/// DESCRIPTION		:
-///		This file is the Aircraft Transmission System, it's job is to read telemetry data, add a check sum and send it to the server over time of 1 message a second
+/// FILE: AircraftTransmissionSystem.cs
+/// PROJECT: SENG3020
+/// PROGRAMMER: SQTeam9
+/// FIRST VERSION: 11/06/2022
+/// DESCRIPTION	:
+///	This file is the Aircraft Transmission System, it's job is to read telemetry data, add a check sum and send it to the server over time of 1 message a second
 ///	
 
 namespace AircraftTransmissionSystem
 {
     using System;
     using System.Net.Sockets;
-    using System.Net;
     using System.Threading;
     class AircraftTransmissionSystem
     {
@@ -19,9 +18,13 @@ namespace AircraftTransmissionSystem
         const int SERVER_PORT = 8989;
         const string SERVER_ADDRESS = "127.0.0.1";
         static string AircraftName = "C-FGAX";
+        public static bool Done = false;
+        static readonly object lockObject = new object();
 
- 
+        /// <summary>
         /// Start Point
+        /// </summary>
+        /// <param name="args">The arguments provided via console</param>
         static void Main(string[] args)
         {
             /// Checks if there is a specific aircraft carrier data to be sent, otherwise use C_FGAX as default via arguments
@@ -53,10 +56,18 @@ namespace AircraftTransmissionSystem
             }
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public AircraftTransmissionSystem(){}
 
+        /// <summary>
         ///Function to calculate CheckSum
         ///Checksum = (ALT + Pitch + Bank) / 3;
+        /// </summary>
+        /// <param name="Alt">Altitude of Telemetry Data</param>
+        /// <param name="Pitch">Pitch of Telemetry Data</param>
+        /// <param name="Bank">Bank of Telemetry Data</param>
         public static int CalculateCheckSum(float Alt, float Pitch, float Bank)
         {
             return (int)(Alt + Pitch + Bank) / 3;
@@ -88,45 +99,39 @@ namespace AircraftTransmissionSystem
             }
         }
 
+        /// <summary>
         /// The Connection method is a thread that sends messages to the ground control terminal
+        /// </summary>
+        /// <param name="o">TCPClient Information</param>
         public static void Connection(Object o)
         {
-            Console.WriteLine("Thread Started");
             TcpClient client = (TcpClient)o;
             NetworkStream stream = null;
 
             /// Get a stream object for reading and writing
             stream = client.GetStream();
 
-            Console.WriteLine("Connection Established");
-
-
             lock (lockObject)
             {
                 string telemetryLine = null;
                 int telemetryCheckSum = 0;
 
-                Console.WriteLine("Attempting to write to server: "+AircraftName);
-
                 try
                 {
-                    //Grabs log contents into a string
+                    /// Grabs log contents into a string
                     uint counter = 0;
                     foreach (string line in System.IO.File.ReadLines("D:/School/Level5/Advanced Software Quality/AircraftTransmissionSystem/AircraftTransmissionSystem/AircraftTransmissionSystem/" + AircraftName + ".txt"))
                     {
-                        Console.WriteLine("We can read lines");
                         string[] flightInformation = null;
                         char[] unwantedChar = { ',' };
 
+                        ///Formatting single line telemetry data
                         telemetryLine = line.Replace(" ", "");
                         flightInformation = telemetryLine.Split(unwantedChar, StringSplitOptions.RemoveEmptyEntries);
-
                         telemetryCheckSum = CalculateCheckSum(float.Parse(flightInformation[5]), float.Parse(flightInformation[6]), float.Parse(flightInformation[7]));
-
                         telemetryLine = AircraftName + "," + counter.ToString() + "," + telemetryLine + telemetryCheckSum.ToString() + ",";
 
                         Console.WriteLine(telemetryLine);
-
 
                         byte[] msg = System.Text.Encoding.ASCII.GetBytes(telemetryLine);
                         stream.Write(msg, 0, msg.Length);
@@ -147,9 +152,5 @@ namespace AircraftTransmissionSystem
             stream.Close();
             client.Close();
         }
-
-        public static bool Done = false;
-        static readonly object lockObject = new object();
-        static TcpListener server = null;
     }
 }
