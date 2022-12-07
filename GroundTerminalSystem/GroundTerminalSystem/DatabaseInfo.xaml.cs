05d47e8900d80442cfd7f85925aa1feeb41fa803
josh murphy <jaymurph92@gmail.com>
2022-12-07 01:39:28 -0500

    Implement "Write to File" functionality

--- GroundTerminalSystem/GroundTerminalSystem/DatabaseInfo.xaml.cs
+++ GroundTerminalSystem/GroundTerminalSystem/DatabaseInfo.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
-
+using FDMS.DAL;
 using GroundTerminalSystem.classes;
 
 namespace GroundTerminalSystem
@@ -19,6 +22,24 @@ namespace GroundTerminalSystem
         {
             DatabaseView.ItemsSource = DatabaseSearch.dbaseSearch(App.SelectionDatabase, SearchTextBox.Text);
             DatabaseView.Items.Refresh();
+        }
+
+        private void LogButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (TextWriter writer = File.CreateText(Path.Combine(Environment.CurrentDirectory, "SearchLog_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt")))
+                {
+                    foreach (TelemetryRecordDal item in (List<TelemetryRecordDal>)(DatabaseView.ItemsSource))
+                    {
+                        writer.WriteLine(item.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
